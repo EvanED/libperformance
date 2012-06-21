85e090b7d0118fe2cea731dba0b519b31932963c
Evan Driscoll <driscoll@cs.wisc.edu>
2012-06-21 11:01:00 -0500

    Move a bunch of files around now that the "Windows" implementation is platform-independent

--- monitor/src/monitor.cpp
+++ monitor/src/monitor.cpp
@@ -1,23 +1,52 @@
 #include <functional>
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include <vector>
-
 #include <cassert>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <signal.h>
-#include <fcntl.h>
+#include <vector>
 
 #include "getmemusage.h"
 
-extern "C" void annotate(const char * str);
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread_time.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/chrono.hpp>
+#include <boost/thread.hpp>
+
+namespace chrono = boost::chrono;
+using boost::thread;
+
+#include "../../../monitor/src/monitor.h"
 
 
 namespace {
+    struct ErrorReturnerWrapper {
+        error_returner_t const func;
+
+        long long operator()() {
+            long long out;
+            int err = func(&out);
+            if (err) {
+                throw "Error!";
+            }
+            return out;
+        }
+
+        ErrorReturnerWrapper(error_returner_t f)
+            : func(f)
+        {}
+
+    private:
+        void operator=(ErrorReturnerWrapper const & other);
+    };
+
+    typedef std::pair<std::function<long long()>, std::string> Tracker;
+    typedef std::function<void (char const*)> Callback;
+
+    std::vector<Tracker> trackers;
+    std::vector<std::function<void (char const*)>> callbacks;
+
     struct DestructionRunner {
         std::function<void ()> callee;
 
@@ -31,148 +60,119 @@ namespace {
         }
     };
 
-    const double nsec_to_sec = 1000000000.0;
-    const int bytes_to_mb = 1024 * 1024;
-    
-    void end();
-    int outfile_fd = -1;
-    timespec start_time;
-
-    std::vector<std::pair<timespec, std::string>> annotations;
-
-    // This function licensced under the MIT license. From
-    // http://www.geonius.com/software/libgpl/ts_util.html.
-    struct  timespec  tsSubtract (struct  timespec  time1,
-                                  struct  timespec  time2)
-    {
-        struct  timespec  result ;
-
-        if ((time1.tv_sec < time2.tv_sec) ||
-            ((time1.tv_sec == time2.tv_sec) &&
-             (time1.tv_nsec <= time2.tv_nsec)))
+    struct ConstructionRunner {
+        template<typename Callable>
+        ConstructionRunner(Callable callee)
         {
-            // TIME1 <= TIME2?
-            result.tv_sec = result.tv_nsec = 0 ;
-        } else {
-            // TIME1 > TIME2
-            result.tv_sec = time1.tv_sec - time2.tv_sec ;
-            if (time1.tv_nsec < time2.tv_nsec) {
-                result.tv_nsec = time1.tv_nsec + 1000000000L - time2.tv_nsec ;
-                result.tv_sec-- ;				/* Borrow a second. */
-            } else {
-                result.tv_nsec = time1.tv_nsec - time2.tv_nsec ;
-            }
+          callee();
         }
-        return (result) ;
-    }
+    };
+
+    const int bytes_to_mb = 1024 * 1024;
+
+    void end();
     
-    
-    void print_prefix()
+    bool start_time_set = false;
+    chrono::system_clock::time_point start_time;
+
+    double elapsed_sec()
     {
-        std::string s = "data = [\n";
-        ssize_t ret = write(outfile_fd, s.c_str(), s.size());
-        assert(ret == s.size());
+        assert(start_time_set);
+        chrono::duration<double> diff = chrono::system_clock::now() - start_time;
+        return diff.count();
     }
 
-    void print_suffix()
+    typedef std::vector<std::pair<double, std::string>> annotation_list;
+    annotation_list annotations;
+
+    void alarm()
     {
         std::stringstream ss;
-        ss << "]\n"
-           << "\n"
-           << "data_t = zip(*data)\n"
-           << "[elapsed, vm_bytes, resident_bytes, vm_bytes_peak, vm_resident_bytes_peak] = data_t\n"
-           << "\n"
-           << "import matplotlib\n"
-           << "matplotlib.use(\"PDF\")\n"
-           << "import matplotlib.pyplot as plt\n"
-           << "plt.plot(elapsed, vm_bytes_peak, \"k\",\n"
-           << "         elapsed, vm_resident_bytes_peak, \"k\",\n"
-           << "         elapsed, vm_bytes, \"r\",\n"
-           << "         elapsed, resident_bytes, \"b\")\n";
+        ss << "{ ";
+        ss << "\"time_offset_sec\": " << elapsed_sec() << ", ";
+        //std::vector<Tracker> trackers;
+        std::for_each(trackers.begin(), trackers.end(),
+                      [&ss](Tracker & t) {
+                          ss << "\"" << t.second << "\": " << t.first() << ", ";
+                      });
 
-        for (auto const & ann : annotations) {
-            double time = ann.first.tv_sec + ann.first.tv_nsec/nsec_to_sec;
-            ss << "plt.annotate(\"" << ann.second << "\", "
-               << "xy=(" << time << ",1), xytext=(" << time << ",1), "
-               << "rotation=\"vertical\", va=\"bottom\", size=\"x-small\")\n";
+        ss << "}";
+
+        std::string const & str = ss.str();
+        char const * cstr = str.c_str();
+
+        // std::vector<Callback> callbacks;
+        std::for_each(callbacks.begin(), callbacks.end(),
+                      [cstr](Callback & cb) {
+                          cb(cstr);
+                     });
+    }
+
+    volatile bool gogogo = true;
+
+    void loop_the_loop()
+    {
+        while(gogogo) {
+          thread::sleep(boost::get_system_time() + boost::posix_time::milliseconds(250));
+          alarm();
         }
-        
-        ss << "plt.savefig(\"blah.pdf\", format=\"pdf\")\n";
-        
-        std::string s = ss.str();
-        ssize_t ret = write(outfile_fd, s.c_str(), s.size());
-        assert(ret == s.size());
     }
 
-    void alarm(int x)
+    thread loop_thread;
+
+    void start()
     {
-        timespec time;
-        clock_gettime(CLOCK_REALTIME, &time);
-        timespec diff = tsSubtract(time, start_time);
-        
-        memory_stats_t m;
-        get_self_memory_usage(&m);
+        start_time_set = true;
+        start_time = chrono::system_clock::now();
 
-        std::stringstream ss;
-        ss << "    ["
-           << diff.tv_sec + diff.tv_nsec/nsec_to_sec << ", "
-           << m.vm_bytes/bytes_to_mb << ", "
-           << m.resident_bytes/bytes_to_mb << ","       
-           << m.vm_bytes_peak/bytes_to_mb << ", "
-           << m.resident_bytes_peak/bytes_to_mb << "],\n";
+        // For testing purposes
+        register_tracker_error_returner(get_self_vm_bytes, "VM bytes");
+        register_tracker_error_returner(get_self_resident_bytes, "RSS bytes");
+        register_tracker_error_returner(get_self_vm_bytes_peak, "VM bytes peak");
+        register_tracker_error_returner(get_self_resident_bytes_peak, "RSS bytes peak");
 
-        std::string s = ss.str();
-        ssize_t ret = write(outfile_fd, s.c_str(), s.size());
-        assert(ret == s.size());
-    }
+        register_monitor_callback([](char const * str) -> void { std::cout << str << "\n"; });
 
-    void start_timer() __attribute__((constructor));    
-    void start_timer()
-    {
-        outfile_fd = creat("blah.py", S_IRUSR | S_IWUSR);
-        assert(outfile_fd != -1);
-        
-        clock_gettime(CLOCK_REALTIME, &start_time);
-        
-        print_prefix();
-        
-        struct itimerval tout_val;
-        
-        signal(SIGALRM, alarm);
-
-        // 0.25sec
-        tout_val.it_interval.tv_sec = 0;
-        tout_val.it_interval.tv_usec = 250 * 1000;
-        tout_val.it_value.tv_sec = 0;
-        tout_val.it_value.tv_usec = 250 * 1000;
-        setitimer(ITIMER_REAL, &tout_val,0);
+        loop_thread = thread(loop_the_loop);
     }
 
     void end()
     {
-        signal(SIGVTALRM, SIG_DFL);
+        gogogo = false;
 
-        annotate("Program end");
-
-        print_suffix();
+        loop_thread.join();
     }
 
+    ConstructionRunner starter(start);
     DestructionRunner ender(end);
 }
 
 void annotate(const char * str)
 {
-    timespec time;
-    clock_gettime(CLOCK_REALTIME, &time);
-    timespec diff = tsSubtract(time, start_time);
-
-    double time_sec = diff.tv_sec + diff.tv_nsec/nsec_to_sec;
-
-    std::stringstream ss;
-    ss << str << " (" << time_sec << "sec)";
-
-    annotations.push_back(std::make_pair(diff, std::string(ss.str())));
+    annotations.push_back(std::make_pair(elapsed_sec(), std::string(str)));
 }
 
+void register_tracker_error_returner(error_returner_t tracker, char const * key)
+{
+    trackers.push_back(Tracker(ErrorReturnerWrapper(tracker), key));
+}
 
+void register_tracker_value_returner(value_returner_t tracker, char const * key)
+{
+    trackers.push_back(Tracker(tracker, key));
+}
 
+void register_monitor_callback(monitor_callback_t callback)
+{
+    callbacks.push_back(callback);
+}
+
+void register_tracker(std::function<long long ()> const & tracker, std::string const & key)
+{
+    trackers.push_back(Tracker(tracker, key));
+}
+
+void register_monitor_callback(std::function<void (char const *)> const & callback)
+{
+    callbacks.push_back(callback);
+}
