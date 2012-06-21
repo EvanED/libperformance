2af0f1eb1c64902fcb489ccf1698d9fe2fd60025
Evan Driscoll <driscoll@cs.wisc.edu>
2012-06-21 18:32:14 -0500

    Use a couple colors

--- grapher/src/grapher.py
+++ grapher/src/grapher.py
@@ -1,5 +1,15 @@
 import json
 
+_next_color = ["r", "b", "g"]
+def next_color():
+    global _next_color
+    try:
+        ret = _next_color[0]
+        _next_color = _next_color[1:]
+        return ret
+    except IndexError:
+        return "k"
+
 with open("blah.json") as input:
     run_info = json.load(input)
     data = run_info["measurements"]
@@ -24,7 +34,7 @@ for key in measurement_keys:
     values = [datum[key] for datum in data]
     assert len(values) == len(elapsed)
 
-    plt.plot(elapsed, values, "k")
+    plt.plot(elapsed, values, next_color())
     
 
 for annotation in annotations:
